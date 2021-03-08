27aa22fdbe6b32ba765503dabbdc623cd1b33933
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-03-08 15:53:56 +0100

    Esercizio palindromo completato

--- js/script.js
+++ js/script.js
@@ -1,13 +1,27 @@
 var wordToReverse = prompt("Inserisci una parola palindroma");
-function palindromeChecker() {
-}
 
 function reverse(word) {
   var wordReversed = [];
   for (var i = 0; i < word.length; i++) {
     wordReversed[(word.length - 1) - i] = word[i];
   }
+  wordReversed = wordReversed.join("");
   return wordReversed;
 }
 
 console.log(reverse(wordToReverse));
+console.log(wordToReverse);
+
+function palindromeChecker(word) {
+  word = word.toLowerCase();
+  if ( reverse(word) == word ) {
+    return true;
+  }
+  return false;
+}
+
+if ( palindromeChecker(wordToReverse) == true ) {
+  console.log("La parola inserita è palindroma");
+} else {
+  console.log("La parola inserita non è palindroma, mi dispiace");
+}
