a338cf3220ea77399b82b01c8043b4f7f1561ae0
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-03-08 16:03:00 +0100

    Aggiunti commenti

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
+// Dichiarazione variabili
 var wordToReverse = prompt("Inserisci una parola palindroma");
 
+// Dichiarazione funzioni
+// Funzione che assegna i caratteri in senso invertito agli slot di un altro array e infine vengono riuniti in un unica stringa per la comparazione
 function reverse(word) {
   var wordReversed = [];
   for (var i = 0; i < word.length; i++) {
@@ -9,9 +12,7 @@ function reverse(word) {
   return wordReversed;
 }
 
-console.log(reverse(wordToReverse));
-console.log(wordToReverse);
-
+// Funzione che restituisce "true" se la parola è uguale quando invertita con un controllo delle maiuscole
 function palindromeChecker(word) {
   word = word.toLowerCase();
   if ( reverse(word) == word ) {
@@ -20,6 +21,11 @@ function palindromeChecker(word) {
   return false;
 }
 
+// Output della parola inserita e invertita
+console.log(reverse(wordToReverse));
+console.log(wordToReverse);
+
+// Output della verifica
 if ( palindromeChecker(wordToReverse) == true ) {
   console.log("La parola inserita è palindroma");
 } else {
