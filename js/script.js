cace82bb88a75c9bd585d8aa7aa1eaddae4ab669
Jacopo Vinanti <jacopovinanti@gmail.com>
2021-03-08 16:10:21 +0100

    Aggiunto bottone palindromeChecker()

--- js/script.js
+++ js/script.js
@@ -1,33 +1,36 @@
-// Dichiarazione variabili
-var wordToReverse = prompt("Inserisci una parola palindroma");
+// PALIDNROME CHECKER
+function palindromeChecker() {
+  // Dichiarazione variabili
+  var wordToReverse = prompt("Inserisci una parola palindroma");
 
-// Dichiarazione funzioni
-// Funzione che assegna i caratteri in senso invertito agli slot di un altro array e infine vengono riuniti in un unica stringa per la comparazione
-function reverse(word) {
-  var wordReversed = [];
-  for (var i = 0; i < word.length; i++) {
-    wordReversed[(word.length - 1) - i] = word[i];
+  // Dichiarazione funzioni
+  // Funzione che assegna i caratteri in senso invertito agli slot di un altro array e infine vengono riuniti in un unica stringa per la comparazione
+  function reverse(word) {
+    var wordReversed = [];
+    for (var i = 0; i < word.length; i++) {
+      wordReversed[(word.length - 1) - i] = word[i];
+    }
+    wordReversed = wordReversed.join("");
+    return wordReversed;
   }
-  wordReversed = wordReversed.join("");
-  return wordReversed;
-}
 
-// Funzione che restituisce "true" se la parola è uguale quando invertita con un controllo delle maiuscole
-function palindromeChecker(word) {
-  word = word.toLowerCase();
-  if ( reverse(word) == word ) {
-    return true;
+  // Funzione che restituisce "true" se la parola è uguale quando invertita con un controllo delle maiuscole
+  function isPalindrome(word) {
+    word = word.toLowerCase();
+    if ( reverse(word) == word ) {
+      return true;
+    }
+    return false;
   }
-  return false;
-}
 
-// Output della parola inserita e invertita
-console.log(reverse(wordToReverse));
-console.log(wordToReverse);
+  // Output della parola inserita e invertita
+  console.log(reverse(wordToReverse));
+  console.log(wordToReverse);
 
-// Output della verifica
-if ( palindromeChecker(wordToReverse) == true ) {
-  console.log("La parola inserita è palindroma");
-} else {
-  console.log("La parola inserita non è palindroma, mi dispiace");
+  // Output della verifica
+  if ( isPalindrome(wordToReverse) == true ) {
+    console.log("La parola inserita è palindroma");
+  } else {
+    console.log("La parola inserita non è palindroma, mi dispiace");
+  }
 }
